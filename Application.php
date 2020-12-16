930e9810863cd9a1f94512987f4a0b60ecba3150
sksaju <sksaaju@gmail.com>
2020-12-16 23:51:13 +0600

    add event listener

--- Application.php
+++ Application.php
@@ -14,47 +14,41 @@ use sksaju\phpmvc\db\Database;
 
 class Application
 {
-    public static string $ROOT_DIR;
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
 
-    public string $layout = 'main';
+    protected array $eventListeners = [];
+
+    public static Application $app;
+    public static string $ROOT_DIR;
     public string $userClass;
+    public string $layout = 'main';
     public Router $router;
     public Request $request;
     public Response $response;
-    public Session $session;
-    public Database $db;
-    public ?UserModel $user;
-    public View $view;
-
-
-    public static Application $app;
     public ?Controller $controller = null;
+    public Database $db;
+    public Session $session;
+    public View $view;
+    public ?UserModel $user;
 
-    /**  
-     * Application constructor.
-     * 
-     * @param string $ROOT_DIR
-     * @param array $config
-     */
-    public function __construct($ROOT_DIR, $config)
+    public function __construct($rootDir, $config)
     {
+        $this->user = null;
         $this->userClass = $config['userClass'];
+        self::$ROOT_DIR = $rootDir;
         self::$app = $this;
-        self::$ROOT_DIR = $ROOT_DIR;
         $this->request = new Request();
         $this->response = new Response();
-        $this->session = new Session();
         $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($config['db']);
+        $this->session = new Session();
         $this->view = new View();
 
-        $this->db = new Database($config['db']);
-
-        $primaryValue = $this->session->get('user');
-        if ($primaryValue) {
-            $primaryKey = $this->userClass::primaryKey();
-            $this->user = $this->userClass::findOne([$primaryKey => $primaryValue]);
-        } else {
-            $this->user = null;
+        $userId = Application::$app->session->get('user');
+        if ($userId) {
+            $key = $this->userClass::primaryKey();
+            $this->user = $this->userClass::findOne([$key => $userId]);
         }
     }
 
@@ -63,40 +57,44 @@ class Application
         return !self::$app->user;
     }
 
-    public function run()
-    {
-        try {
-            echo $this->router->resolve();
-        } catch (\Exception $e) {
-            $this->response->setStatusCode($e->getCode());
-            echo $this->view->renderView('_error', [
-                'exception' => $e
-            ]);
-        }
-    }
-
-    public function getController()
-    {
-        return $this->controller;
-    }
-
-    public function setController(Controller $controller)
-    {
-        $this->controller = $controller;
-    }
-
     public function login(UserModel $user)
     {
         $this->user = $user;
         $primaryKey = $user->primaryKey();
-        $primaryValue = $user->{$primaryKey};
-        $this->session->set('user', $primaryValue);
+        $value = $user->{$primaryKey};
+        Application::$app->session->set('user', $value);
+
         return true;
     }
 
     public function logout()
     {
         $this->user = null;
-        $this->session->remove('user');
+        self::$app->session->remove('user');
+    }
+
+    public function run()
+    {
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
+        try {
+            echo $this->router->resolve();
+        } catch (\Exception $e) {
+            echo $this->router->renderView('_error', [
+                'exception' => $e,
+            ]);
+        }
+    }
+
+    public function triggerEvent($eventName)
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+        foreach ($callbacks as $callback) {
+            call_user_func($callback);
+        }
+    }
+
+    public function on($eventName, $callback)
+    {
+        $this->eventListeners[$eventName][] = $callback;
     }
 }
